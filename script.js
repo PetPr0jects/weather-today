72437a3f0497167e2e54be01e539e539d97a9980
PetPr0jects <practiceofpet@gmail.com>
2022-05-30 20:27:29 +0600

    Added script to get weather by city name used 2 api's so request time might be little long. And decided to make request on enter so it required one more function. Litle bit of html for result field and styling as well. Other than that planing to make aimated background and animated result for the user tomorrow.

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputButton = document.getElementById("inputButton")
+const userInput = document.getElementById("userInput")
+inputButton.addEventListener('click', getWeatherBtn);
+userInput.addEventListener('keydown', getWeather)
+async function getWeather(){
+    if(event.key === 'Enter'){
+    let userInput = document.getElementById("userInput").value;
+    let userRequest = "https://nominatim.openstreetmap.org/search/"+userInput+"?format=json";
+    const responseFromApi = await fetch(userRequest);
+    const data = await responseFromApi.json();
+    const responseToUser = data[0];
+    const lon = responseToUser.lon;
+    const lat = responseToUser.lat;
+    let weatherRequest = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=0add19c65d238de8a19d9686c17f47f4"
+    const responseFromWeather = await fetch(weatherRequest);
+    const dataWeather = await responseFromWeather.json();
+    document.getElementById("result").innerHTML = dataWeather.weather[0].main + " | " +Number((dataWeather.main.temp-273.15).toFixed(1)) + "°C";
+}}
+async function getWeatherBtn(){
+    let userInput = document.getElementById("userInput").value;
+    let userRequest = "https://nominatim.openstreetmap.org/search/"+userInput+"?format=json";
+    const responseFromApi = await fetch(userRequest);
+    const data = await responseFromApi.json();
+    const responseToUser = data[0];
+    const lon = responseToUser.lon;
+    const lat = responseToUser.lat;
+    let weatherRequest = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=0add19c65d238de8a19d9686c17f47f4"
+    const responseFromWeather = await fetch(weatherRequest);
+    const dataWeather = await responseFromWeather.json();
+    document.getElementById("result").innerHTML = dataWeather.weather[0].main + " | " +Number((dataWeather.main.temp-273.15).toFixed(1)) + "°C";
+}
